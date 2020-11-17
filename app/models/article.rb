460ca9c94f18beb4dd3eb8bb8d35ebf339be7116
seki_yuts <>
2020-11-18 06:32:20 +0900

    seed修正

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,18 +3,28 @@
 # Table name: articles
 #
 #  id         :integer          not null, primary key
-#  content    :text
-#  title      :string
+#  content    :text             not null
+#  title      :string           not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  user_id    :integer          not null
+#
+# Indexes
+#
+#  index_articles_on_user_id  (user_id)
 #
 class Article < ApplicationRecord
     validates :title, presence: true
-    #lendth文字の長さの指定するときに使う
-    #minimumは最低文字数、maximumは最高文字数
+    # =>titleがないと保存しませんよ
+    # =>ApplicationRecordは保存しないがSQLは保存してもいいよって状態
+
+    
     validates :title, length: { minimum: 2, maximum: 100 }
-    #formatは特定の文字を含ませたい時などに使う
+    # =>lendth文字の長さの指定するときに使う
+    # =>minimumは最低文字数、maximumは最高文字数
+    
     validates :title, format: { with: /\A(?!\@)/ }
+    # =>formatは特定の文字を含ませたい時などに使う
 
     validates :content, presence: true
     validates :content, length: { minimum: 10}
@@ -25,8 +35,15 @@ class Article < ApplicationRecord
     #メゾット名を書いてprivateに実行するメゾットをかく
     validate :validate_title_and_content_length
 
+    #belongs_to :　　　はarticleとuserは紐づいてることを表す
+    belongs_to :user
+
     def display_created_at
         I18n.l(self.created_at, format: :default)
+    end
+
+    def author_name
+        user.display_name
     end
 
     private
