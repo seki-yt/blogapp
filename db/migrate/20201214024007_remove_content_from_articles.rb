4f90eedb2a5306753cdb6bf47131ab248fa33d91
seki_yuts <>
2020-12-16 14:08:19 +0900

    細かな修正

--- db/migrate/20201214024007_remove_content_from_articles.rb
+++ db/migrate/20201214024007_remove_content_from_articles.rb
@@ -2,4 +2,16 @@ class RemoveContentFromArticles < ActiveRecord::Migration[6.0]
   def change
     remove_column :articles, :content, :text
   end
+
+  # def up
+  #   remove_column :articles, :content
+  # end
+
+  # => migrationを実行したときにのみ実行される
+
+  # def down
+  #   add_column :articles, :content, text
+  # end
+
+  # => rollbackされたときのみ実行される
 end
